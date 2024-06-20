18a51ae69299f306db9af9b8fe85e64a7d1ee313
dev-Dylan09 <yellek@protonmail.com>
2024-06-19 22:58:26 -0400

    added endpoint handler and file server for the /app/ path

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// Readiness endpoint handler
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	fileServer := http.FileServer(http.Dir("."))
+	// File server for the /app/* path
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	mux.Handle("/", fileServer)
 
