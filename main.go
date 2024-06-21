455a9248ba8465c2816f3ef160505c3a7ce4920b
dev-Dylan09 <yellek@protonmail.com>
2024-06-20 23:47:34 -0400

    added chirp,error,and valid struct. Added validatechirphandler function.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,18 @@ import (
 type apiConfig struct {
 	fileserverHits int
 	mu             sync.Mutex
+}
+
+type ChirpRequest struct {
+	Body string 'json:"body"'
+}
+
+type ErrorResponse struct {
+	Error string 'json:"error"'
+}
+
+type ValidResponse struct {
+	Valid bool 'json:"valid"'
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -67,6 +80,36 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var chirpRequest ChirpRequest
+	err := json.NewDecoder(r.Body).Decode(&chirpRequest)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encdoe(ErrorResponse{Error: "Invalid request body"})
+		return
+	}
+
+	if len(chirpRequest.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Chirp is too long"})
+		return
+	}
+
+	if len(chirpRequest.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Chirp is too long"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(ValidResponse{Valid: true})
 }
 
 func main() {
