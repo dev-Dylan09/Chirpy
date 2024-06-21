6629f4631c9f6c433806ed26fa3a04aa29f92c87
dev-Dylan09 <yellek@protonmail.com>
2024-06-20 21:15:42 -0400

    added struct to hold data, fileserverhits counter, handler to display hits, and handler to reset hits.

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type apiConfig struct {
+	fileserverHits int
+	mu             sync.Mutex
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.mu.Lock()
+		cfg.fileserverHits++
+		cfg.mu.Unlock()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0\n"))
+}
 
 func main() {
 	const port = "8080"
