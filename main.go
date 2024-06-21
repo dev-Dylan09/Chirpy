976d5b78ced1bfd90766f8cd2873ae461159be27
dev-Dylan09 <yellek@protonmail.com>
2024-06-20 22:01:54 -0400

    added healthz handler to check http method and added check for http method for apiConfig method

--- main.go
+++ main.go
@@ -22,6 +22,10 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 	cfg.mu.Lock()
 	defer cfg.mu.Unlock()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -35,6 +39,16 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0\n"))
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
 func main() {
