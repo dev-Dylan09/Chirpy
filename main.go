a87df7ba944821d0e2067a8af8867d3f3102eecb
dev-Dylan09 <yellek@protonmail.com>
2024-06-20 22:07:26 -0400

    modified readiness endpoint handler

--- main.go
+++ main.go
@@ -58,14 +58,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// readiness endpoint handler
 	mux.HandleFunc("/healthz", healthzHandler)
-
-	// Readiness endpoint handler
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
 
 	fileServer := http.FileServer(http.Dir("."))
 	// File server for the /app/* path
